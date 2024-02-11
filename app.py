12a3f57c30f700cb3c7743035e724d9a322fcdfe
ervenderr <ervenidjad12@gmail.com>
2024-02-12 03:26:00 +0800

    requirements.txt

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def submit():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
